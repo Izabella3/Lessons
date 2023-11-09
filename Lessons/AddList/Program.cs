1cd8119b1d081b427c109fc3c626f9c233bb37aa
Izabella3 <bella_s@purlin.com>
2023-11-09 14:50:28 +0400

    Seperate odd and even numbers

---
+++ Lessons/AddList/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+List<int> numbers = new List<int>() { 45, 46, 78, 59, 453 };
+List<int> oddNumbers = new List<int>();
+List<int> evenNumbers = new List<int>();
+
+
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        evenNumbers.Add(number);
+    }
+    else
+    {
+        oddNumbers.Add(number);
+    }
+}
+
+Console.WriteLine("Odd Numbers");
+
+foreach (var oddNumber in oddNumbers)
+{ 
+    Console.WriteLine(oddNumber); 
+}
+Console.WriteLine("Even Numbers");
+foreach (var evenNumber in evenNumbers)
+{
+    Console.WriteLine(evenNumber);
+}
+
+Console.ReadKey();
